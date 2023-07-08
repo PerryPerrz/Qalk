a2dbf0a4afb4fc99331c504887666408efe5cd53
PerryPerrz <iophugstud6@gmail.com>
2023-07-08 18:36:38 +0200

    Mise en place des calculs

--- index.js
+++ index.js
@@ -6,45 +6,59 @@ document.querySelector('.icon').addEventListener('mouseleave', function () {
   document.querySelector('.modal').style.display = 'none';
 });
 
-// Fixing the value
+// Fixing the values
 const brutHourValue = 11.52,
   netHourValue = 9.11;
 
-// Get the value of the input with id="brut"
-const brutHour = document.getElementById('brut'),
-  netHour = document.getElementById('net');
+const brutHour = document.getElementById('brutHour'),
+  netHour = document.getElementById('netHour'),
+  nbHours = document.getElementById('nbHours'),
+  netSalary = document.getElementById('netSalary'),
+  brutSalary = document.getElementById('brutSalary');
 
-// By default, the brutHour input is 11,52
+// By default
 brutHour.value = brutHourValue;
-
-// By default, the netHour input is 9,11
 netHour.value = netHourValue;
 
-// If the user click on the input, it clear the value, but if the user click outside the input, the value is back to 11,52
+// If the user click on the input, it clear the value
 brutHour.addEventListener('focus', function () {
   brutHour.value = '';
 });
 
-// If the user click on the input, it clear the value, but if the user click outside the input, the value is back to 9,11
+// If the user click on the input, it clear the value
 netHour.addEventListener('focus', function () {
   netHour.value = '';
 });
 
-// If the user click outside the input and he didn't fill the field, the value is back to 11,52
+// If the user click outside the input and he didn't fill the field, the value is back to it's default value
 brutHour.addEventListener('focusout', function () {
   if (brutHour.value === '') {
     brutHour.value = brutHourValue;
   }
 });
 
-// If the user click outside the input and he didn't fill the field, the value is back to 9.11
+// If the user click outside the input and he didn't fill the field, the value is back to it's default value
 netHour.addEventListener('focusout', function () {
   if (netHour.value === '') {
     netHour.value = netHourValue;
   }
 });
 
+// If the user change any value in the whole document, it calculate the salary again
+document.addEventListener('change', function () {
+  calculate();
+});
+
 // Function to calculate the salary
 function calculate() {
+  calculateBrut();
+  calculateNet();
+}
 
+function calculateBrut() {
+  netSalary.value = netHour.value * nbHours.value;
+}
+
+function calculateNet() {
+  brutSalary.value = brutHour.value * nbHours.value;
 }
