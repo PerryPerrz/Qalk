8ad03c6f021903e57414ca9c975b329cee596192
PerryPerrz <iophugstud6@gmail.com>
2023-07-09 17:19:13 +0200

    Réinitialisation, sauvegarde et chargement

--- index.js
+++ index.js
@@ -62,3 +62,61 @@ function calculateBrut() {
 function calculateNet() {
   brutSalary.value = brutHour.value * nbHours.value;
 }
+
+// When the user click on the save button, it create a JSON file and save all the values of the inputs inside it
+document.querySelector('.exportBtn').addEventListener('click', function () {
+  const data = {
+    brutHour: brutHour.value,
+    netHour: netHour.value,
+    nbHours: nbHours.value,
+    netSalary: netSalary.value,
+    brutSalary: brutSalary.value,
+  };
+
+  const a = document.createElement('a');
+  a.href = URL.createObjectURL(
+    new Blob([JSON.stringify(data, null, 2)], {
+      type: 'application/json',
+    })
+  );
+  a.setAttribute('download', 'data.json');
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+});
+
+// When the user click on the load button, it load the JSON file and fill the inputs with the values
+document.querySelector('.loadBtn').addEventListener('click', function () {
+  const input = document.createElement('input');
+  input.type = 'file';
+  input.accept = 'application/json';
+  input.onchange = (event) => {
+    processFile(event.target.files[0]);
+  };
+  input.click();
+});
+
+function processFile(file) {
+  const reader = new FileReader();
+  reader.readAsText(file, 'UTF-8');
+  reader.onload = function (evt) {
+    const data = JSON.parse(evt.target.result);
+    brutHour.value = data.brutHour;
+    netHour.value = data.netHour;
+    nbHours.value = data.nbHours;
+    netSalary.value = data.netSalary;
+    brutSalary.value = data.brutSalary;
+  }
+  reader.onerror = function (evt) {
+    alert('error reading file');
+  }
+}
+
+// When the user click on the reset button, it reset all the values to their default value
+document.querySelector('.resetBtn').addEventListener('click', function () {
+  brutHour.value = brutHourValue;
+  netHour.value = netHourValue;
+  nbHours.value = 0;
+  netSalary.value = 0;
+  brutSalary.value = 0;
+});
